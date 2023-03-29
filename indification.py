cd5c5dc9221428a4e4cd565bd28ebaf53548e7a9
danlylacov <99447254+danlylacov@users.noreply.github.com>
2023-03-29 15:18:24 +0300

    Update indification.py

--- indification.py
+++ indification.py
@@ -2,21 +2,39 @@ from audio import audio_main, draw_grafic
 
 
 def sity_lengh(p_data, q_data): # расстояние городских кварталов
-    res = 0
-    for i in range(min(len(p_data), len(q_data))):
-        res += abs(p_data[i] - q_data[i])
-    return res
+    res_list = []
+    for i in range(len(p_data)):
+        res = 0
+        for j in range(min(len(p_data[i]), len(q_data[i]))):
+            res += abs(p_data[i][j] - q_data[i][j])
+        res_list.append(res)
+    return res_list, sum(res_list)
 
-dan1 = audio_main('audio1\Record (online-voice-recorder.com)--online-audio-convert.com.wav')
-dan2 = audio_main('audio1\Record (online-voice-recorder.com) (2)--online-audio-convert.com.wav')
-andr1 = audio_main('audio1\Record (online-voice-recorder.com) (3)--online-audio-convert.com.wav')
-andr2 = audio_main('audio1\Record (online-voice-recorder.com) (4)--online-audio-convert.com.wav')
-dan3 = audio_main('audio1\Record (online-voice-recorder.com) (5)--online-audio-convert.com.wav')
-al1 = audio_main('audio1\Record (online-voice-recorder.com) (6)--online-audio-convert.com.wav')
-al2 = audio_main('audio1\Record (online-voice-recorder.com) (7)--online-audio-convert.com.wav')
-print(sity_lengh(dan1, dan2))
-print(sity_lengh(al1,  dan1))
-print(sity_lengh(al1, al2))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
